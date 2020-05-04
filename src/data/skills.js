34d4dcfcf4ef9bf0e5a972bf03096931c00809bf
JakeAdmire <jakeadmire1@gmail.com>
2020-05-04 11:24:08 -0600

    Add skills data

---
+++ src/data/skills.js
@@ -0,0 +1,50 @@
+module.exports = {
+    skills: [
+        {
+            title: 'Web Design & Development',
+            icon: null,
+            related: [
+                'HTML',
+                'CSS',
+                'JavaScript',
+                'TypeScript',
+            ],
+        },
+        {
+            title: 'Front-End Frameworks',
+            icon: null,
+            related: [
+                'Angular #',
+                'React',
+                'Gatsby',
+                'Vue',
+            ],
+        },
+        {
+            title: 'Back-End Frameworks',
+            icon: null,
+            related: [
+                'Node.js',
+                'Express.js',
+            ],
+        },
+        {
+            title: 'UI/UX Design',
+            icon: null,
+            related: [
+                'Adobe XD',
+                'Figma',
+                'Sketch',
+                'Gravit',
+            ],
+        },
+        {
+            title: 'Database Management System',
+            icon: null,
+            related: [
+                'PostgreSQL',
+                'SOQL',
+            ],
+        },
+    ]
+}
