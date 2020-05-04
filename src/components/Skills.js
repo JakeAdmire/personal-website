9cd0f099d4aa6b697a77cfca1ffe564326e80bda
JakeAdmire <jakeadmire1@gmail.com>
2020-05-04 14:31:57 -0600

    Wireframe basic skill card

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,13 +1,36 @@
 import React from "react";
-import skills from '../data/skills';
+
+import * as Icons from "./Icons";
+import skills from "../data/skills";
+
 
 const Skills = () => {
+    const renderSkill = skill => {
+        const Icon = Icons[skill.icon];
 
-  return (
-    <section>
+        return (
+            <div className="m-auto">
+                <div    className="flex items-center border rounded-lg py-1  pr-4"
+                        style={{ width: '20rem' }}
+                        id="skill-card">
+                    <span className="text-2xl px-4">
+                        <Icon />
+                    </span>
+                    <span>
+                        <p>{skill.title}</p>
+                        <p className="opacity-50 text-sm">({skill.related})</p>
+                    </span>
+                </div>
+            </div>
+        );
+    }
 
-    </section>
-  )
+    return (
+        <section    className="grid row-gap-3 col-gap-1"
+                    style={{ gridTemplateColumns: 'repeat(4, minmax(320px, 1fr))' }}>
+            {skills.map(skill => renderSkill(skill))}
+        </section>
+    );
 }
 
 export default Skills
