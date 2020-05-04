3506c99bfc4d1490982f4139eb1e065d9504ed9e
JakeAdmire <jakeadmire1@gmail.com>
2020-05-04 11:23:57 -0600

    Create base component

---
+++ src/components/Icons.js
@@ -0,0 +1,15 @@
+import {
+    FaHtml5,
+    FaReact,
+    FaNodeJs,
+    FaPencilAlt,
+    FaDatabase
+} from "react-icons/fa";
+
+export {
+    FaHtml5,
+    FaReact,
+    FaNodeJs,
+    FaPencilAlt,
+    FaDatabase
+};
