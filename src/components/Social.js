d3c2b38892b94c7a15999a21c303e2a64166cd19
JakeAdmire <jakeadmire1@gmail.com>
2020-05-06 13:48:49 -0600

    Wireframe basic social icons

--- src/components/Social.js
+++ src/components/Social.js
@@ -1,9 +1,29 @@
 import React from "react";
 
-const Social = () => {
+import social from "../data/social.json";
+import * as Icons from "./Icons";
+
+const Social = ({ isTypingDone }) => {
 
     return (
-        <a></a>
+        <div className={`flex ${isTypingDone ? 'opacity-100' : 'opacity-0'}`}>
+            {social.map((ref, i) => {
+                const Icon = Icons[ref.icon];
+
+                return (
+                    <a  key={ref.title}
+                        href={ref.link}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className={`m-1 p-3 bg-gray-300 rounded-full ${isTypingDone ? 'transition-opacity duration-300 opacity-100' : 'opacity-0'}`}
+                        style={{ transitionDelay: `${i * 0.5 + 1}s` }}
+                        data-tip={ref.title}
+                        data-place="bottom">
+                        <Icon />
+                    </a>
+                );
+            })}
+        </div>
     );
 }
 
