b52b624653ab3d21bfbbbafe322c601defe74642
JakeAdmire <jakeadmire1@gmail.com>
2020-05-05 18:26:05 -0600

    Wireframe basic hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import Typist from "react-typist";
 
 const Hero = () => {
+    const [isTypingDone, setTypingStatus] = useState(false);
 
-  return (
-    <header></header>
-  );
+    return (
+        <header>
+            <Typist cursor={{ show: false }}
+                    avgTypingDelay={60}
+                    onTypingDone={() => setTypingStatus(true)}
+                    className="h-56 my-3">
+                <h2 className="text-5xl">Hey there!</h2>
+                <Typist.Delay ms={300} />
+                <h1 className="text-6xl font-medium">I'm Jake Admire,</h1>
+                <Typist.Delay ms={300} />
+                <h3 className="text-5xl">UI Developer</h3>
+                <Typist.Delay ms={300} />
+            </Typist>
+            <p className={`transition duration-1000 ${isTypingDone ? 'opacity-50' : 'opacity-0'}`}>
+                Front-End Developer / UI Designer
+            </p>
+            {/* incrementally load links */}
+        </header>
+    );
 }
 
 export default Hero;
